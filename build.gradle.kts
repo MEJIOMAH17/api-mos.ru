04d59e865bff2a0e8727689d7a939f94e6f79bc3
Mark <mekosichkin@gmail.com>
2021-01-05 09:22:14 -0800

    ....

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-
 plugins {
     java
     kotlin("jvm") version "1.4.0"
+    `maven-publish`
 }
 
 group = "com.github"
@@ -18,12 +18,33 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.8.1")
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5+")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5+")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5+") // for kotest framework
     testImplementation("io.kotest:kotest-runner-junit5:4+") // for kotest framework
     testImplementation("io.kotest:kotest-assertions-core:4+") // for kotest core jvm assertions
 }
 
+val githubToken: String by project
+val githubUser: String by project
+
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/MEJIOMAH17/api-mos.ru")
+            credentials {
+                username = githubUser
+                password = githubToken
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
